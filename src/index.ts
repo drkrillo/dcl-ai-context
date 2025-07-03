b7656499ad52cdf248e205f9c27905da00f656a7
= <=>
2025-07-03 14:41:53 -0300

    Add UI boilerplate code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import {} from '@dcl/sdk/math'
 import { engine } from '@dcl/sdk/ecs'
+import { ReactEcsRenderer } from '@dcl/sdk/react-ecs'
+import { uiMenu } from './ui'
 
-export function main() {}
+export function main() {
+    ReactEcsRenderer.setUiRenderer(uiMenu)
+}
